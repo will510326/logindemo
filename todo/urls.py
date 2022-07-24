c497834e5e454287973022dd02e6f664547e6f72
Wayne <Wayne@gmail.com>
2022-07-24 16:13:57 +0800

    finish register workflow

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.todo, name='todo'),
-    path('view/<int:id>', views.view, name='view')
+    path('view/<int:id>', views.view, name='view'),
+    path('create/', views.create, name='create'),
+    path('completed/', views.completed, name='completed'),
+    path('delete/<int:id>', views.delete, name='delete'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,68 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from tenacity import retry
 from .models import Todo
+from .forms import TodoForm
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+def delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    todo.delete()
+
+    return redirect('todo')
+
+
+@login_required
+def completed(request):
+    todos = Todo.objects.filter(user=request.user, completed=True)
+    return render(request, './todo/completed.html', locals())
+
+
+@login_required
+def create(request):
+    form = TodoForm()
+    message = ''
+    try:
+        if request.method == 'POST':
+            form = TodoForm(request.POST)
+            if form.is_valid():  # form表單是否符合合法(設定標準)
+                todo = form.save(commit=False)  # 因沒有user資訊，故先將commit=False先不儲存
+                todo.user = request.user
+                todo.save()  # 可以將資料儲存到資料庫
+                return redirect('todo')
+            else:
+                message = '新增失敗'
+    except Exception as e:
+        print(e)
+    return render(request, './todo/create.html', locals())
+
+
+@login_required
 def view(request, id):
     # todo = Todo.objects.filter(id=id)
+    message = ''
     todo = get_object_or_404(Todo, id=id)
+    form = TodoForm(instance=todo)  # 取的表單資訊
+    if request.method == 'POST':
+        if request.POST.get('delete'):
+            todo.delete()
+            return redirect('todo')
+
+        if request.POST.get('update'):
+            message = '更新失敗'
+            try:
+                form = TodoForm(request.POST, instance=todo)
+                if form.is_valid():
+                    todo = form.save(commit=False)
+                    todo.date_completed = datetime.today().strftime('%Y-%m-%d %H:%M:%S')\
+                        if todo.completed else None
+                    todo.save()
+                    return redirect('todo')
+            except Exception as e:
+                print(e)
+
     return render(request, './todo/view.html', locals())
 
 

6229e6e2633aa9c9e47275488b38bc80c540257e
Wayne <Wayne@gmail.com>
2022-08-06 22:49:33 +0800

    finish bulma web

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7ivcrwrikpgkj=)3ey+54hose#dz2##@6gl^!o3z3topgn)=0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
